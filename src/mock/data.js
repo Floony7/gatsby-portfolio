b9cd44199af8a67e3661879ba809443e27934428
Floony7 <fred.lunjevich@stretchsense.com>
2023-10-26 21:38:58 +0100

    Added Hand Engine project

--- src/mock/data.js
+++ src/mock/data.js
@@ -26,17 +26,18 @@ export const aboutData = {
   'I have reignited my love of Next.js and enjoying the process of getting back into the Jamstack universe, having also worked with Gatsby over the years.',  
   // resume: // if no resume, the button will not show up
 };
+const inactive = "Project no longer active";
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'periodic.jpg',
-    title: 'React Periodic table App',
-    info: 'Project that uses React and spedifically the Context API to produce a visualisation of the elements of the Periodic Table. Previously, this project used a  third-party API, however I created a new API with Node.js for the purposes of owning the data and persisting it as long as I liked.',
-    info2: '',
-    url: 'https://objective-franklin-4f675c.netlify.app/',
-    repo: 'https://github.com/Floony7/periodic-table-app-react-context', // if no repo, the button will not show up
+    img: 'hand-engine-focused.png',
+    title: 'Hand Engine software',
+    info: 'Hand Engine motion capture glove software - StretchSense.',
+    info2: 'UX and UI design and development, Redux state implementation, creation of info panels, features, windows, custom hand implementations (Three.js).',
+    url: 'https://stretchsense.com/',
+    repo: 'Proprietary Software',
   },
   {
     id: nanoid(),
@@ -44,26 +45,8 @@ export const projectsData = [
     title: 'React Countries App (in development)',
     info: 'Using a coutries API to pull data through on countries and the regions they are situated. Uses React Context API and React Router to display country pages.',
     info2: '',
-    url: 'https://vibrant-almeida-61fe32.netlify.app/',
-    repo: 'https://github.com/Floony7/react-countries-reference-app', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'fpl.jpg',
-    title: 'Fantasy Premier League Captain Selector',
-    info: 'Vanilla JavaScript app that is designed to take in player names and produce a random selection. Project uses local storage to store player names added by the user.',
-    info2: '',
-    url: 'https://elated-shaw-b31750.netlify.app/',
-    repo: 'https://github.com/Floony7/fantasy-premier-league-captain-selector', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'dad.jpg',
-    title: 'React Dad Jokes App (in development)',
-    info: 'Dad Jokes generator that will add jokes to Local Storage and have upvoting and downvoting functionality (yet to be added). The UI is primarily made from Tailwind CSS.',
-    info2: '',
-    url: 'https://gallant-haibt-5b9dc5.netlify.app/',
-    repo: '', // if no repo, the button will not show up
+    url: `${inactive}`,
+    repo: 'https://github.com/Floony7/react-countries-reference-app',
   },
 ];
 
